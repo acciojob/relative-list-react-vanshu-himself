b1aed6ad5e94ac27376926e4b0160058306f8663
Vansh Gupta <you@example.com>
2023-07-18 12:34:46 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+             <ol style={{ key:"relativeList"}} >
+              <li style={{ key:"relativeListItem1"}}>
+                bua ji
+              </li>
+              <li style={{ key:"relativeListItem2"}}>
+     fufa ji
+</li>
+<li style={{ key:"relativeListItem3"}}>
+ taya ji
+</li>
+<li style={{ key:"relativeListItem4"}}>
+papa ji
+</li>
+              </ol>
     </div>
   )
 }
